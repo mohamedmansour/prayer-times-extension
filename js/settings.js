e3976d6fe314bd7fbd8ebb1bb7297038981a3716
Mohamed Mansour <hello@mohamedmansour.com>
2011-03-06 14:30:04 -0500

    Refactor out Settings from Background Page.

---
+++ js/settings.js
@@ -0,0 +1,38 @@
+// Global Settings.
+settings = {
+  get version() {
+    return localStorage['version'];
+  },
+  set version(val) {
+    localStorage['version'] = val;
+  },
+  get currentPosition() {
+    var key = localStorage['currentPosition'];
+    return (typeof key == 'undefined') ? null : key;
+  },
+  set currentPosition(val) {
+    localStorage['currentPosition'] = val;
+  },
+  get timeformat() {
+    var key = localStorage['timeformat'];
+    return (typeof key == 'undefined') ? '24h' : key;
+  },
+  set timeformat(val) {
+    localStorage['timeformat'] = val;
+  },
+  get calculation() {
+    var key = localStorage['calculation'];
+    return (typeof key == 'undefined') ? 'Jafari' : key;
+  },
+  set calculation(val) {
+    localStorage['calculation'] = val;
+  },
+  get timenames() {
+    var key = localStorage['timenames'];
+    return (typeof key == 'undefined') ?
+        ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'] : key.split(',');
+  },
+  set timenames(val) {
+    localStorage['timenames'] = val;
+  },
+};
