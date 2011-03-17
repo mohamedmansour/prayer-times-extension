95e7ef1e8a268c7e722b084a1c8a28dd962ef571
DmitriBabaev <babaevdmitri@gmail.com>
2011-03-17 15:46:19 +0300

    Added full working timer

--- js/AlarmCalcs.js
+++ js/AlarmCalcs.js
@@ -1,65 +1,61 @@
-function AlarmClock (){
+var AlarmCalcs = function  (){
     var NextPrayer = {
         "time" : "",
         "name" : "",
-        "type" : 2, //0 - намаз, 1 - не намаз, 2 - не инициализировано
-        "delta" : 1000 //время оставшееся до
+        "type" : 2, //0 - prayer, 1 - not prayer, 2 - initial value
+        "delta" : 1000 //time to prayer
     };
     var CurrentPrayer = {
         "time" : "",
         "name" : "",
         "type" : 2, //0 - намаз, 1 - не намаз, 2 - не инициализировано
-        "delta" : 1000
+        "delta" : 1000 //time from prevous prayer
     }  /*  Устанавливаем параметры
                  */
-    var i = 0;
     var flag = 0; //0 - еще не выдавались сообщения о наступлении нового времени
-    window.setInterval(
-        function(){
-            var times = entity.getTimes();
-            var timenames = entity.getTimeNames();
-            var list = settings.timenames;
-            for(var i in list) {
-                var listItem = list[i].toLowerCase();
-                var minutes_namaz =  sTimeMinutes(times[listItem]);
-                var minutes_ctime =  cTimeMinites();
-                var delta;
-                var delta1;
-                delta = minutes_namaz - minutes_ctime; // ищем следующее время
-                //ищем текущее время
-                delta1 = minutes_ctime - minutes_namaz;
-                if ((delta<NextPrayer.delta) && (delta>0)){
-                    NextPrayer.delta = delta;
-                    NextPrayer.time = times[listItem];
-                    NextPrayer.name = timenames[listItem];
-                }
-                if ((delta1<CurrentPrayer.delta) && (delta1>0)){
-                    CurrentPrayer.delta = delta1;
-                    CurrentPrayer.time = times[listItem];
-                    CurrentPrayer.name = timenames[listItem];
-                }
+    var nextTimeInfo = function(){
+        return (NextPrayer.name + " " + NextPrayer.delta);
+    }
+    var currentTimeInfo = function(){
+        return (CurrentPrayer.name + " " + CurrentPrayer.delta);
+    }
+    var flagInfo  = function(){
+        return (flag);
+    }
+    var init = function(){
+        var i = 0;
+        var times = entity.getTimes();
+        var timenames = entity.getTimeNames();
+        var list = settings.timenames;
+        NextPrayer.delta = 10000;
+        CurrentPrayer.delta = 10000;
+        for(var i in list) {
+            var listItem = list[i].toLowerCase();
+            var minutes_namaz =  sTimeMinutes(times[listItem]);
+            var minutes_ctime =  cTimeMinites();
+            var delta;
+            var delta1;
+            delta = minutes_namaz - minutes_ctime; // ищем следующее время
+            //ищем текущее время
+            delta1 = minutes_ctime - minutes_namaz;
+            if ((delta<NextPrayer.delta) && (delta>=0)){
+                NextPrayer.delta = delta;
+                NextPrayer.time = times[listItem];
+                NextPrayer.name = timenames[listItem];
             }
-            if (is_namaz_time(NextPrayer.name)){
-                NextPrayer.type = 0
-            } else {
-                NextPrayer.type = 1
+            if ((delta1<CurrentPrayer.delta) && (delta1>=0)){
+                CurrentPrayer.delta = delta1;
+                CurrentPrayer.time = times[listItem];
+                CurrentPrayer.name = timenames[listItem];
             }
-            if (is_namaz_time(CurrentPrayer.name)){
-                CurrentPrayer.type = 0
-            } else {
-                CurrentPrayer.type = 1
-            }
-            if((CurrentPrayer.delta<10) && (flag ==0)){
-                makeAlarm(CurrentPrayer.name, chrome.i18n.getMessage('nowPrayerTime')+ " " + CurrentPrayer.name);
-                flag = 1;
-            }
-            if(CurrentPrayer.delta>10){
-                flag = 0;
-            }
-        },
-        2000
-        );
-
+        }
+        if(CurrentPrayer.delta<10){
+            makeAlarm(CurrentPrayer.name, chrome.i18n.getMessage('nowPrayerTime')+ " " + CurrentPrayer.name);
+        } else {
+            flag = 0;
+        }
+        window.setTimeout(init, 60001);
+    }
     function is_namaz_time (namaz){
         var namaz_array = [
         "fajr", "dhuhr", "asr", "maghrib", "isha"
@@ -86,20 +82,23 @@ function AlarmClock (){
 
     function makeAlarm(title, body, type){
         // Create a simple text notification:
-        var notification = webkitNotifications.createNotification(
-            'img/icon48.png',  // icon url - can be relative
-            title,  // notification title
-            body  // notification body text
-            );
+        if (flag == 0) {
+            var notification = webkitNotifications.createNotification(
+                'img/icon48.png',  // icon url - can be relative
+                title,  // notification title
+                body  // notification body text
+                );
 
-        // Then show the notification.
-        notification.show();
-    }
-    function setAlarm(){
-
-    }
-    return {
+            // Then show the notification.
+            notification.show();
+            flag = 1;
+        }
         
     }
-
+    return {
+        initAlarm: init,
+        currentTime: currentTimeInfo,
+        nextTime:nextTimeInfo,
+        flagShow:flagInfo
+    }
 }
