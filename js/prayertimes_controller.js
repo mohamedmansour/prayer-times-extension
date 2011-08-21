5753c659256d31a1a83897d7644190316a1ccc39
Mohamed Mansour <hello@mohamedmansour.com>
2011-08-21 13:10:40 -0400

    Remove onUpdate from the list.

--- js/prayertimes_controller.js
+++ js/prayertimes_controller.js
@@ -90,7 +90,7 @@ PrayerTimesController.prototype.onInstall = function() {
 PrayerTimesController.prototype.onUpdate = function() {
   // Do not send updates if the user is opt'd out!
   if (!settings.opt_out) {
-    chrome.tabs.create({url: 'updates.html'});
+    // chrome.tabs.create({url: 'updates.html'});
   }
 };
 
