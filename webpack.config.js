4856e8aa08a4f7696cf0fa229cefb7c5105b147b
Mohamed Mansour <hello@mohamedmansour.com>
2018-07-01 23:28:47 -0700

    Initial webpack template
    Core template that follows webpack + typescript bundling.
    
    Issue #19

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/background.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: 'background.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
