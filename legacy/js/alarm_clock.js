263c9e191581aec9cedeedc1b535d55b7028a291
Mohamed Mansour <hello@mohamedmansour.com>
2019-04-07 13:41:08 -0700

    Fix notifications not clearing when clicked

--- legacy/js/alarm_clock.js
+++ legacy/js/alarm_clock.js
@@ -6,8 +6,7 @@
  *   Emad Al-Shihabi
  *   Mohamed Mansour
  */
-AlarmClock = function(entity)
-{
+AlarmClock = function (entity) {
   this.entity = entity;
   this.times = null;
   this.nowPrayerTimeLabel = chrome.i18n.getMessage('nowPrayerTime');
@@ -17,11 +16,8 @@ AlarmClock = function(entity)
   this.athanNotificationId = 'notification-id';
   this.athanPlayer = new AthanPlayer();
 
-  chrome.notifications.onClosed.addListener(callback => {
-    this.athanPlayer.stopAthan();
-  })
-
   chrome.notifications.onClicked.addListener(callback => {
+    this.athanPlayer.stopAthan();
     chrome.notifications.clear(this.athanNotificationId);
   })
 };
@@ -30,7 +26,7 @@ AlarmClock = function(entity)
  * Sets the athan sound for this alaram.
  * @param{string} audioValue The alarm sound text.
  */
-AlarmClock.prototype.setAthan = function(audioValue) {
+AlarmClock.prototype.setAthan = function (audioValue) {
   var audioSource = audioValue.substring(1);
   var audioType = audioValue.substring(0, 1) == '1' ? 'Shia' : 'Sunni';
   this.athanPlayer.setAthanTrack(audioType, audioSource);
@@ -39,41 +35,38 @@ AlarmClock.prototype.setAthan = function(audioValue) {
 /**
  * Update the local cache of the prayer time names.
  */
-AlarmClock.prototype.setPrayerTimeNames = function(times)
-{
+AlarmClock.prototype.setPrayerTimeNames = function (times) {
   this.prayerTimeNames = times;
 };
 
 /**
  * Resets the Alarm Clock to Fajr.
  */
-AlarmClock.prototype.reset = function()
-{
+AlarmClock.prototype.reset = function () {
   this.times = this.entity.getTimes(null, '24h');
   var timeName = this.prayerTimeNames[0];
   var time = this.times[timeName.toLowerCase()];
   this.nextPrayerTime = {
-      time : time,
-      name : timeName,
-      delta : (1.0 * this.dateStringInMinutes(time) + 24 * 60 - this.currentTimeInMinutes())
+    time: time,
+    name: timeName,
+    delta: (1.0 * this.dateStringInMinutes(time) + 24 * 60 - this.currentTimeInMinutes())
   };
 };
 
 /**
  * Starts the Alarm Clock to figure out when the next prayer is.
  */
-AlarmClock.prototype.start = function()
-{
+AlarmClock.prototype.start = function () {
   this.reset();
   // Current time in minutes from day beginning
-  var crMinutes =  this.currentTimeInMinutes();
+  var crMinutes = this.currentTimeInMinutes();
   var delta = -1;
   for (var i in this.prayerTimeNames) {
     var timeName = this.prayerTimeNames[i];
     var time = this.times[timeName.toLowerCase()];
 
     // Prayer time in minutes from day beginning.
-    var prMinutes =  this.dateStringInMinutes(time);
+    var prMinutes = this.dateStringInMinutes(time);
     delta = prMinutes - crMinutes;
 
     // If delta became > 0 then we found next prayer.
@@ -81,7 +74,7 @@ AlarmClock.prototype.start = function()
       this.nextPrayerTime.delta = delta;
       this.nextPrayerTime.name = timeName;
       this.nextPrayerTime.time = time;
-      
+
       // It is time to show the notification!
       if (delta == 0) {
         if (!this.alarmActive) {
@@ -89,11 +82,10 @@ AlarmClock.prototype.start = function()
           this.makeAlarm(this.getNextPrayerName(), prayerNotificationLabel + ' ' + this.getNextPrayerName());
           this.alarmActive = true;
         }
-      }
-      else {
+      } else {
         this.alarmActive = false;
       }
-      
+
       break;
     }
   }
@@ -104,8 +96,7 @@ AlarmClock.prototype.start = function()
 /**
  * The Current Time in Minutes.
  */
-AlarmClock.prototype.currentTimeInMinutes = function()
-{
+AlarmClock.prototype.currentTimeInMinutes = function () {
   var d = new Date();
   var h = d.getHours();
   var m = d.getMinutes();
@@ -115,8 +106,7 @@ AlarmClock.prototype.currentTimeInMinutes = function()
 /**
  * From the given date string, returns the number of minutes.
  */
-AlarmClock.prototype.dateStringInMinutes = function(s)
-{
+AlarmClock.prototype.dateStringInMinutes = function (s) {
   var h = 1.0 * s.split(':')[0];
   var m = 1.0 * s.split(':')[1];
   return 60 * h + m;
@@ -129,18 +119,17 @@ AlarmClock.prototype.dateStringInMinutes = function(s)
  * @param {string} body The description that will be shown in the body of the 
  *                      notification.
  */
-AlarmClock.prototype.makeAlarm = function(title, body)
-{
+AlarmClock.prototype.makeAlarm = function (title, body) {
   if (!settings.notificationVisible) {
     return;
   }
 
   chrome.notifications.create(this.athanNotificationId, {
     type: 'basic',
-    iconUrl: 'img/icon48.png', 
+    iconUrl: 'img/icon48.png',
     title: title,
     message: body,
-    requireInteraction: true
+    requireInteraction: false
   }, (callback) => {
     if (settings.athanVisible) {
       this.athanPlayer.playAthan();
@@ -152,22 +141,24 @@ AlarmClock.prototype.makeAlarm = function(title, body)
  * Creates and updates the Chrome Badge Text.
  * Always change the title, but decide whether to show the badge.
  */
-AlarmClock.prototype.makeBadge = function()
-{
-  chrome.browserAction.setTitle({title: this.getNextPrayerName()});
-  
+AlarmClock.prototype.makeBadge = function () {
+  chrome.browserAction.setTitle({
+    title: this.getNextPrayerName()
+  });
+
   if (!settings.badgeVisible) {
     return;
   }
 
-  chrome.browserAction.setBadgeText({text: this.getNextPrayerTime()});
+  chrome.browserAction.setBadgeText({
+    text: this.getNextPrayerTime()
+  });
 };
 
 /**
  * Formatted time to next prayer
  */
-AlarmClock.prototype.getNextPrayerTime = function()
-{
+AlarmClock.prototype.getNextPrayerTime = function () {
   var h = Math.floor(this.nextPrayerTime.delta / 60);
   var m = this.nextPrayerTime.delta - 60 * h;
   var s = (m < 10) ? (h + ':0' + m) : (h + ':' + m);
@@ -177,7 +168,6 @@ AlarmClock.prototype.getNextPrayerTime = function()
 /**
  * Name of next prayer
  */
-AlarmClock.prototype.getNextPrayerName = function()
-{
+AlarmClock.prototype.getNextPrayerName = function () {
   return this.nextPrayerTime.name;
 };
