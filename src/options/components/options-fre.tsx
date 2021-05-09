0ec377d320d10fe188d2b31f499c792e9f003311
Mohamed Mansour <hello@mohamedmansour.com>
2021-05-08 20:04:38 -0700

    Start doing some options

--- src/options/components/options-fre.tsx
+++ src/options/components/options-fre.tsx
@@ -1,21 +1,46 @@
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
+import { calculationMethods, CalculationName, PrayerTimeFormat } from '../../shared/pray_time'
 import { useOptionsState } from '../state'
 
 export const FirstRunExperience = observer(() => {
   const state = useOptionsState()
 
+  if (!state.settings) {
+    return <>Loading...</>
+  }
+
   return <>
     <h1>FRE</h1>
-    {state.coordinates && (
+
+    <h2>Location</h2>
+    {state.settings.currentPosition && (
       <div>
         <p>
-          <strong>Latitude:</strong> {state.coordinates.latitude} &nbsp;
-          <strong>Longitude:</strong> {state.coordinates.longitude}
+          <strong>Latitude:</strong> {state.settings.currentPosition.latitude} &nbsp;
+          <strong>Longitude:</strong> {state.settings.currentPosition.longitude}
         </p>
       </div>
     )}
-    
+
     <button onClick={() => state.queryLocation()}>Refresh Location</button>
+
+    <h2>Calculation Method</h2>
+    <select value={state.settings.calculation}>
+      {Object.keys(calculationMethods).map((method) => (
+        <option key={method} value={method}>{calculationMethods[method].name}</option>
+      ))}
+    </select>
+
+    <h2>Time Format</h2>
+    <select value={state.settings.timeformat}>
+      <option value={PrayerTimeFormat.TwelveHourFormat}>12 hours</option>
+      <option value={PrayerTimeFormat.TwentyFourFormat}>24 hours</option>
+    </select>
+
+    <h2>Visible Times</h2>
+    {state.settings.timenames.map((timename, idx) => (
+      <span key={idx}><input value={timename} type="checkbox" /> {timename}</span>
+    ))}
   </>
 })
--- src/options/state/options-state.ts
+++ src/options/state/options-state.ts
@@ -1,11 +1,64 @@
 import { makeAutoObservable, runInAction } from 'mobx'
 import { browser } from 'webextension-polyfill-ts'
+import { CalculationName, PrayerTimeFormat } from '../../shared/pray_time'
 
 type PageType = 'fre' | 'settings'
 
+async function getSetting<T>(keys: string[]) {
+  const values = await browser.storage.sync.get(keys)
+  const results = {}
+  keys.forEach(key => {
+    let result = undefined
+    const value = values[key]
+
+    switch (key) {
+      case 'version':
+      case 'currentPosition':
+        result = value
+        break
+      case 'timeformat':
+        result = (typeof value == 'undefined') ? PrayerTimeFormat.TwelveHourFormat : PrayerTimeFormat[value]
+        break
+      case 'calculation':
+        result = (typeof value == 'undefined') ? CalculationName.Jafari : CalculationName[value]
+        break
+      case 'timenames':
+        result = (typeof value == 'undefined') ? [
+          'imsak',
+          'fajr',
+          'sunrise',
+          'dhuhr',
+          'asr',
+          'sunset',
+          'maghrib',
+          'isha',
+          'midnight'
+        ] : value.split(',')
+        break
+      // case 'bypassCache':
+      //   result = value == true
+      //   break
+      default:
+        result = undefined
+        break
+    }
+
+    results[key] = result
+  })
+
+  return results as T
+}
+
+interface Settings {
+  currentPosition: GeolocationCoordinates
+  calculation: CalculationName
+  timeformat: PrayerTimeFormat
+  timenames: string[]
+}
+
 export class OptionsState {
   page: PageType = 'fre'
-  coordinates: Partial<GeolocationCoordinates> = {}
+  settings: Settings = null
 
   constructor() {
     makeAutoObservable(this, {})
@@ -13,8 +66,15 @@ export class OptionsState {
   }
 
   async init() {
-    const { coordinates } = await browser.storage.sync.get(['coordinates'])
-    runInAction(async () => this.coordinates = coordinates)
+    const settings = await getSetting<Settings>([
+      'currentPosition',
+      'timeformat',
+      'calculation',
+      'timenames'
+    ])
+    runInAction(async () => {
+      this.settings = settings
+    })
   }
 
   queryLocation() {
@@ -22,8 +82,8 @@ export class OptionsState {
       navigator.geolocation.getCurrentPosition(
         async (position: GeolocationPosition) => {
           const { latitude, longitude } = position.coords
-          await browser.storage.sync.set({'coordinates': { latitude, longitude }})
-          runInAction(() => this.coordinates = position.coords)
+          await browser.storage.sync.set({'currentPosition': { latitude, longitude }})
+          runInAction(() => this.settings.currentPosition = position.coords)
         },
         (positionError: GeolocationPositionError) => {
           console.error('Geolocation Error', positionError)
