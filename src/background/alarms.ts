c7b9fb697edd9e0228570e9d6c572465a23cd28b
Mohamed Mansour <hello@mohamedmansour.com>
2021-05-15 17:04:31 -0700

    Roll over alarms to the next day when completed

--- src/background/alarms.ts
+++ src/background/alarms.ts
@@ -5,6 +5,7 @@ import { getSetting, Setting, Settings } from '../shared/settings'
 export async function init() {
   browser.storage.onChanged.addListener(async (settings) => await onSettingsChanged(settings))
   browser.alarms.onAlarm.addListener(alarm => onAlarm(alarm))
+  await refreshAllAlarms()
 }
 
 async function onSettingsChanged(settings: Settings) {
@@ -19,7 +20,10 @@ async function onSettingsChanged(settings: Settings) {
 
 async function refreshAllAlarms() {
   await browser.alarms.clearAll()
-  
+  await createAlarmsForDay(new Date())
+}
+
+async function createAlarmsForDay(date: Date) {
   const settings = await getSetting([
     Setting.calculation,
     Setting.currentPosition,
@@ -27,7 +31,6 @@ async function refreshAllAlarms() {
   ])
 
   // Reset to beginning of the day so we can get the number of ms
-  const date = new Date()
   date.setHours(0, 1, 0, 0)
 
   const prayTimesProvider = new PrayTimesProvider(settings.calculation)
@@ -36,6 +39,8 @@ async function refreshAllAlarms() {
   })
 
   Object.keys(settings.timenames).forEach(timename => {
+    if (!settings.timenames[timename])
+      return
     const time = times[timename]
     browser.alarms.create(timename, {
       when: date.getTime() + (time * 60 * 60 * 1000)
@@ -43,6 +48,24 @@ async function refreshAllAlarms() {
   })
 }
 
-function onAlarm(alarm: Alarms.Alarm) {
-  console.log('Prayer Time is now!', alarm.name)
+async function onAlarm(alarm: Alarms.Alarm) {
+  const currentDate = new Date()
+
+  // Alarams that are set in the past are automatically triggered here
+  // So set a threshold of a minute, any alarms older than that wont
+  // be notified.
+  if ((currentDate.getTime() - alarm.scheduledTime) > 60*1000) {
+    console.log('Prayer Time was in past!', alarm.name)
+  } else {
+    console.log('Prayer Time is now!', alarm.name)
+  }
+  
+  // Check if there are any more remaining alarms, if not that implies that
+  // all the alarams for today have been completed. Let's setup the alarms
+  // for tomorrow.
+  const remainingAlarms = await browser.alarms.getAll()
+  if (!remainingAlarms.length) {
+    currentDate.setDate(currentDate.getDate() + 1)
+    await createAlarmsForDay(currentDate)
+  }
 }
